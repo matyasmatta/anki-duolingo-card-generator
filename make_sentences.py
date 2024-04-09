8dcd488e83488ea4908f759909b12e981be3293a
Matyáš Matta <matyasmatta@email.cz>
2024-04-09 16:12:39 +0000

    first llm build

--- make_sentences.py
+++ make_sentences.py
@@ -1,6 +1,7 @@
 import random
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
-
+import argparse
+import time
 
 def generate_model(text, num_sentences, temperature=0.7, top_k=50, max_length=50):
     """
@@ -33,7 +34,7 @@ def generate_model(text, num_sentences, temperature=0.7, top_k=50, max_length=50
         sentences.append(sentence.strip())
     return sentences
 
-def generate_sentences(num, level, seed_iterations=4):
+def generate_sentences(name, num, level, seed_iterations=4):
     match level:
         case "a1":
             with open(f"model/{level}.txt", "r") as f:
@@ -42,7 +43,6 @@ def generate_sentences(num, level, seed_iterations=4):
             for i in range(seed_iterations):
                 seed = data[random.randint(1,len(data)-1)]
                 sentences += (generate_model(seed, num, temperature=0.8, top_k=20, max_length=20))
-            print(sentences)
             sentences = [clean_sentence(sentence) for sentence in sentences]
             sentences = [item for item in sentences if (len(item) < 70 and '"' not in item)]
         case "b2":
@@ -56,7 +56,7 @@ def generate_sentences(num, level, seed_iterations=4):
             seed = data[random.randint(1,len(data)-1)]
             sentences = generate_model(seed, num)
     random.shuffle(sentences)
-    with open("model/sample.txt", "a") as f:
+    with open(f"export/{name}.txt", "a") as f:
         for sentence in sentences:
             f.write(sentence+"\n")
 
@@ -72,4 +72,19 @@ def clean_sentence(item):
         else:
             return item
 
-generate_sentences(10, "a1", seed_iterations=10)
+def main():
+    parser = argparse.ArgumentParser(description='Generate sentences in English.')
+    parser.add_argument('seed_iterations', type=int, help='How seeds in total you want to use.')
+    parser.add_argument('sentences_per_seed', type=int, help='How many sentences per seed should be generated.')
+    parser.add_argument('cefr_level', type=str, help='CEFR language level of the sentences.')
+    
+    args = parser.parse_args()
+    name = f"generated_sentences_{args.seed_iterations}_{args.sentences_per_seed}_{args.cefr_level}_{time.strftime('%Y%m%d-%H%M%S')}"
+
+
+    generate_sentences(name, args.sentences_per_seed, args.cefr_level, seed_iterations=args.seed_iterations)
+    print(f"Textfile '{name}.txt' has been created successfully in folder 'export'.")
+
+
+if __name__ == "__main__":
+    main()
